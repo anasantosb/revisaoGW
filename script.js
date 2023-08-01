5613c4fc7284637482c2915f91f5c0c6ada0046b
anasantosb <134284706+anasantosb@users.noreply.github.com>
2023-08-01 08:41:03 -0300

    a

---
+++ script.js
@@ -0,0 +1,14 @@
+function incognita() {
+    var x = parseInt(document.getElementById("valorx").value);
+    var resultadoElement = document.getElementById("resultado");
+    
+    if (x === numeroAleatorio) {
+    resultadoElement.textContent = "Parabéns! Você acertou o número. Jogue mais uma vez!";
+    } else if (x < numeroAleatorio) {
+    resultadoElement.textContent = "O número digitado é MENOR do que o número gerado para adivinhação. Tente mais uma vez.";
+    } else {
+    resultadoElement.textContent = "O número digitado é MAIOR do que o número gerado para adivinhação. Tente mais uma vez.";
+    }
+}
+    
+    var numeroAleatorio = Math.floor(Math.random() * 100) + 1;
