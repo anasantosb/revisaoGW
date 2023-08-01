51a6be5d971bc0dffa7635f7824c2c0b82afcfb5
anasantosb <134284706+anasantosb@users.noreply.github.com>
2023-08-01 08:47:59 -0300

    Update script.js
    Co-authored-by: ProfKeRibeiro <133296861+ProfKeRibeiro@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function incognita() {
     resultadoElement.textContent = "Parabéns! Você acertou o número. Jogue mais uma vez!";
     } else if (x < numeroAleatorio) {
     resultadoElement.textContent = "O número digitado é MENOR do que o número gerado para adivinhação. Tente mais uma vez.";
+resultadoElement.textContent = `numero incorreto, tente dnv `
     } else {
     resultadoElement.textContent = "O número digitado é MAIOR do que o número gerado para adivinhação. Tente mais uma vez.";
     }
